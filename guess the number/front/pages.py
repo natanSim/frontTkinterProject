226384fb47f21b4adc1669df81ca5f7a7cb4a356
natanSim <nati2004ns@gmail.com>
2024-07-15 17:52:44 +0300

    Clear screen after each window

--- guess the number/front/pages.py
+++ guess the number/front/pages.py
@@ -25,21 +25,23 @@ def open_first_page(window):
 
 def open_second_page(window):
     clear_frame_from_window(window)
+
     window_frame = Frame(master=window, width=500, height=500)
 
     label_of_player_name = Label(master=window_frame, text="Insert your full name: ", font=('Helvetica', 22))
-    label_of_player_name.place(x=120, y=180)
+    label_of_player_name.place(x=120, y=130)
 
     text_box_of_player_name = Entry(master=window_frame)
-    text_box_of_player_name.place(x=185, y=250)
+    text_box_of_player_name.place(x=185, y=200)
 
     button_continue = Button(master=window_frame, text="continue", font=('Helvetica', 12), command=lambda: open_third_page(window))
-    button_continue.place(x=215, y=450)
+    button_continue.place(x=215, y=400)
 
     window_frame.place(x=0, y=80)
 
 def open_third_page(window):
     clear_frame_from_window(window)
+    window_frame = Frame(master=window, width=500, height=500)
 
     # region player details
     label_name = Label(master=window, text="Name: natan", font=('Helvetica', 12))
@@ -59,10 +61,16 @@ def open_third_page(window):
     text_box_of_number.place(x=185, y=270)
 
     button_continue = Button(master=window, text="continue", font=('Helvetica', 12), command=lambda: open_page_fourth(window))
-    button_continue.place(x=215, y=450)
+    button_continue.place(x=215, y=480)
+
+    window_frame.place(x=0, y=80)
 
 
 def open_page_fourth(window):
+    clear_frame_from_window(window)
+
+    window_frame = Frame(master=window, width=500, height=500)
+
     label_of_winner = Label(master=window, text="The winner is:", font=('Helvetica', 24))
     label_of_winner.place(x=145, y=220)
 
@@ -70,7 +78,9 @@ def open_page_fourth(window):
     label_of_player_name.place(x=185, y=260)
 
     button_exit = Button(master=window, text="Exit", font=('Helvetica', 12), command=lambda: close_window(window))
-    button_exit.place(x=215, y=450)
+    button_exit.place(x=225, y=480)
+
+    window_frame.place(x=0, y=60)
 
 def close_window(window):
     window.destroy()
